5714909d9a323af80a75be663aa2412a9b44b17d
HirushaR <hirusharandunu11@gmail.com>
2019-10-24 10:12:03 +0530

    bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,25 @@ class LoginScreen  extends StatelessWidget {
       appBar: AppBar(
       ),
       body: Center(
-        child: Text("hellow"),
+       
       ),
+       bottomNavigationBar: BottomNavigationBar(
+       currentIndex: 0, // this will be set when a new tab is tapped
+       items: [
+         BottomNavigationBarItem(
+           icon: new Icon(Icons.home),
+           title: new Text('Home'),
+         ),
+         BottomNavigationBarItem(
+           icon: new Icon(Icons.mail),
+           title: new Text('Messages'),
+         ),
+         BottomNavigationBarItem(
+           icon: Icon(Icons.person),
+           title: Text('Profile')
+         )
+       ],
+     ),
       
     );
   }
