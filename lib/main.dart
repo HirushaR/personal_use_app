64c6c2561367d44516a4fdeb1e94fcbc485b3830
HirushaR <hirusharandunu11@gmail.com>
2019-10-24 12:56:38 +0530

    grid view

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -9,6 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
    
       home: LoginScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -20,28 +22,68 @@ class LoginScreen  extends StatelessWidget {
       appBar: AppBar(
         
       ),
-      body: Center(
-       
+      body: GridView.count(
+        primary: false,
+  padding: const EdgeInsets.all(20),
+  crossAxisSpacing: 10,
+  mainAxisSpacing: 10,
+  crossAxisCount: 2,
+  children: <Widget>[
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Icon(Icons.home),
+      color: Colors.teal[100],
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Icon(Icons.home),
+      color: Colors.teal[200],
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Icon(Icons.home),
+      color: Colors.teal[300],
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Icon(Icons.home),
+      color: Colors.teal[400],
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Icon(Icons.home),
+      color: Colors.teal[500],
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Icon(Icons.home),
+      color: Colors.teal[600],
+    ),
+  ],
       ),
+      
        bottomNavigationBar: BottomNavigationBar(
-         backgroundColor: Color.fromRGBO(102, 88, 155, 1),
+        
+       backgroundColor: Color.fromRGBO(67, 71, 99, 0.9),
        currentIndex: 0, // this will be set when a new tab is tapped
        items: [
+         
          BottomNavigationBarItem(
-           icon: new Icon(Icons.home),
-           title: new Text('Home'),
+           icon: new Icon(Icons.calendar_today),
+           title: new Text(''),
+          
          ),
          BottomNavigationBarItem(
-           icon: new Icon(Icons.mail),
-           title: new Text('Messages'),
+
+           icon: new Icon(Icons.graphic_eq),
+           title: new Text(''),
          ),
          BottomNavigationBarItem(
            icon: Icon(Icons.person),
-           title: Text('Profile')
+           title: Text('')
          )
        ],
      ),
-      
     );
     
   }
